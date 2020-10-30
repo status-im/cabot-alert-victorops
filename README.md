779a2be97f67116b8f3dd0dc30f80e3b1264a41a
Jakub Sokołowski <jakub@status.im>
2020-10-30 14:33:45 +0100

    add plugin skeleton files
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

---
+++ README.md
@@ -0,0 +1,3 @@
+# Description
+
+This is a [Cabot Alert Plugin](https://cabotapp.com/dev/writing-alert-plugins.html) for sending alerts to [VictorOps](https://victorops.com/).
---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='cabot-alert-victorops',
+      version='0.1.0',
+      description='A VictorOps plugin for Cabot',
+      author='Jakub Sokołowski',
+      author_email='jakub@status.im',
+      url='https://github.com/status-im/cabot-alert-victorops',
+      packages=find_packages(),
+      download_url = 'https://github.com/status-im/cabot-alert-victorops/tarball/0.1.0',
+     )
