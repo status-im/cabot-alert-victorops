762a47d6351d706e37070e2a2dd89b9fb72afb6c
Jakub Sokołowski <jakub@status.im>
2020-10-30 17:10:32 +0100

    first working version
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- cabot_alert_victorops/models.py
+++ cabot_alert_victorops/models.py
@@ -1,9 +1,6 @@
-import requests
+from requests import request
 from os import environ as env
 from cabot.cabotapp.alert import AlertPlugin
-
-from logging import getLogger
-logger = getLogger(__name__)
 
 VICTOROPS_URL = 'https://api.victorops.com/api-public/v1'
 
@@ -14,35 +11,48 @@ class VictorOpsAlertPlugin(AlertPlugin):
     version = "0.1.0"
 
     def send_alert(self, service, users, duty_officers):
-        message = service.get_status_message()
+        message = "TODO"
         for user in users:
-            logger.info('{} - {} - This is bad for your {}.'.format(
-                user, message,
-                u.cabot_alert_victorops_settings.favorite_bone))
-
-            _send_victorops_alert(user, message, "DETAILS")
+            print('Notification for: {} - {}'.format(user, message))
+            self._send_victorops_alert(user, message, "DETAILS")
 
         return True
 
     def _send_victorops_alert(self, user, message, details):
-        app_id = env.get('VICTOROPS_APP_ID')
-        api_key = env.get('VICTOROPS_API_KEY')
+        self.username = env.get('VICTOROPS_USERNAME')
+        if self.username is None:
+            raise Exception("VICTOROPS_USERNAME env variable not found!")
 
+        policy = self._get_policy()
         data = {
             "summary": message,
             "details": details,
-            "userName": user,
-            "isMultiResponder": true,
-            "targets": [],
+            "userName": self.username,
+            "targets": [{
+                "type": "EscalationPolicy",
+                "slug": policy["slug"],
+            }],
         }
-        headers = {
-            "X-VO-Api-Id": app_id,
-            "X-VO-Api-Key": api_key,
-        }
-
-        resp = requests.post(
-            '{}/api-public/v1/incidents'.format(VICTOROPS_URL),
-            data, headers
-        )
+        resp = self._query('POST', 'incidents', data)
         resp.raise_for_status()
         return True
+
+    def _get_policy(self):
+        resp = self._query('GET', 'policies')
+        return resp.json()["policies"][0]["policy"]
+
+    def _query(self, method, path, data={}):
+        self.app_id = env.get('VICTOROPS_APP_ID')
+        self.api_key = env.get('VICTOROPS_API_KEY')
+        if self.app_id is None:
+            raise Exception("VICTOROPS_APP_ID env variable not found!")
+        if self.api_key is None:
+            raise Exception("VICTOROPS_API_KEY env variable not found!")
+
+        headers = {
+            "X-VO-Api-Id": self.app_id,
+            "X-VO-Api-Key": self.api_key,
+        }
+        resp = request(method, VICTOROPS_URL+'/'+path, json=data, headers=headers)
+        resp.raise_for_status()
+        return resp
