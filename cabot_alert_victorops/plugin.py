29cc437b1f109ac24fb79987246049c8d9629472
Jakub Sokołowski <jakub@status.im>
2020-10-30 14:54:50 +0100

    implement basic VictorOps support
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- cabot_alert_victorops/plugin.py
+++ cabot_alert_victorops/plugin.py
@@ -1,29 +1,49 @@
-from cabot.plugins.models import AlertPlugin
-from django import forms
+import requests
 from os import environ as env
+from cabot.plugins.models import AlertPlugin
 
 from logging import getLogger
 logger = getLogger(__name__)
 
-class SkeletonAlertUserSettingsForm(forms.Form):
-    favorite_bone = forms.CharField(max_length=100)
+VICTOROPS_URL = 'https://api.victorops.com/api-public/v1'
 
-class SkeletonAlertPlugin(AlertPlugin):
-    name = "Skeleton"
-    slug = "cabot_alert_skeleton"
-    author = "Jonathan Balls"
-    version = "0.0.1"
+class VictorOpsAlertPlugin(AlertPlugin):
+    name = "VictorOps"
+    slug = "cabot_alert_victorops"
+    author = "Jakub Sokołowski"
+    version = "0.1.0"
     font_icon = "fa fa-code"
 
-    user_config_form = SkeletonAlertUserSettingsForm
-
     def send_alert(self, service, users, duty_officers):
-        calcium_level = env.get('CALCIUM_LEVEL') 
         message = service.get_status_message()
-        for u in users:
-            logger.info('{} - This is bad for your {}.'.format(
-                message,
-                u.cabot_alert_skeleton_settings.favorite_bone))
+        for user in users:
+            logger.info('{} - {} - This is bad for your {}.'.format(
+                user, message,
+                u.cabot_alert_victorops_settings.favorite_bone))
+
+            _send_victorops_alert(user, message, "DETAILS")
 
         return True
 
+    def _send_victorops_alert(self, user, message, details):
+        app_id = env.get('VICTOROPS_APP_ID')
+        api_key = env.get('VICTOROPS_API_KEY')
+
+        data = {
+            "summary": message,
+            "details": details,
+            "userName": user,
+            "isMultiResponder": true,
+            "targets": [],
+        }
+        headers = {
+            "X-VO-Api-Id": app_id,
+            "X-VO-Api-Key": api_key,
+        }
+
+        resp = requests.post(
+            '{}/api-public/v1/incidents'.format(VICTOROPS_URL),
+            data, headers
+        )
+        resp.raise_for_status()
+        return True
